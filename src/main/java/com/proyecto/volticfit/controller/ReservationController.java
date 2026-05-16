1dde987084db3733538d79c5878fd06e705102aa
Miguediaz811 <miguelangeldiaz1307ovi@gmail.com>
2026-05-16 18:37:51 -0500

    fix: se realizaron cambios en los endpoints

--- src/main/java/com/proyecto/volticfit/controller/ReservationController.java
+++ src/main/java/com/proyecto/volticfit/controller/ReservationController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +33,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/reservations")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
 @Tag(name = "Reservations", description = "Gym shift reservation endpoints")
 public class ReservationController {
  
