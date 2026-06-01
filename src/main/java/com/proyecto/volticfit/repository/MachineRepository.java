ce1f74069269b830ab63a47cebd5288c11e77500
Miguel Nieto <mangelngonzalez@gmail.com>
2026-06-01 16:15:34 -0500

    feat: se ha creado el crud de las maquinas y se han agregado sus metodos para buscarlas por nombre y id

--- src/main/java/com/proyecto/volticfit/repository/MachineRepository.java
+++ src/main/java/com/proyecto/volticfit/repository/MachineRepository.java
@@ -3,13 +3,25 @@ package com.proyecto.volticfit.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.proyecto.volticfit.entity.Machine;
 
-@Repository
-public interface MachineRepository extends JpaRepository<Machine, Long> {
-     
-        Optional<Machine> findByNameIgnoreCase(String name);
-    
+
+/**
+ * Repositorio encargado de
+ * la persistencia de máquinas.
+ *
+ * @author Miguel
+ * @version 1.0
+ */
+public interface MachineRepository
+        extends JpaRepository<Machine, Long> {
+
+    /**
+     * Busca una máquina por nombre.
+     *
+     * @param nombre nombre máquina
+     * @return máquina encontrada
+     */
+    Optional<Machine> findByName(String nombre);
 }
