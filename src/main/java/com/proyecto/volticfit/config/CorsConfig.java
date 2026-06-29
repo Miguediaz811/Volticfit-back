eee7b67f25627b7f390ab6adf06124028998cceb
AndresVilla2008 <andresvillamorales2008@gmail.com>
2026-06-29 15:49:08 -0500

    fix: se agrego la ruta permitida para realizar peticiones

--- src/main/java/com/proyecto/volticfit/config/CorsConfig.java
+++ src/main/java/com/proyecto/volticfit/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://volticfit-front.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
