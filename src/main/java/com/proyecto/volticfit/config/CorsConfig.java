86b11bdb7153209ee9d0b25e2f01b7a34fe2956f
AndresVilla2008 <andresvillamorales2008@gmail.com>
2026-06-30 20:57:28 -0500

    fix: correccion de la recuperacion de contraseña

--- src/main/java/com/proyecto/volticfit/config/CorsConfig.java
+++ src/main/java/com/proyecto/volticfit/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://volticfit-front.vercel.app/", "http://localhost:4200/")
+                        .allowedOrigins("https://volticfit-front.vercel.app", "http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
